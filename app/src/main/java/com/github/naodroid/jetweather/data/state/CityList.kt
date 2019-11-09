e1e85c2137a42076b12e7af6892af688e3b0f30b
naodroid <naodroid@gmail.com>
2019-11-09 10:10:52 +0900

    Use coroutine in model layer

--- app/src/main/java/com/github/naodroid/jetweather/data/state/CityList.kt
+++ app/src/main/java/com/github/naodroid/jetweather/data/state/CityList.kt
@@ -6,40 +6,44 @@ import androidx.compose.model
 import androidx.compose.state
 import androidx.compose.unaryPlus
 import androidx.ui.core.ContextAmbient
+import androidx.ui.core.CoroutineContextAmbient
 import com.github.naodroid.jetweather.data.entity.City
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 fun useCityList(): List<City> {
     val context = +ambient(ContextAmbient)
     val state = +state { emptyList<City>() }
+    val coroutineCtx = +ambient(CoroutineContextAmbient)
+    val scope = CoroutineScope(coroutineCtx)
 
     +model {
-        //how to cancel this job?
-        GlobalScope.launch {
-            val list = readCityList(context)
-            GlobalScope.launch(Dispatchers.Main) {
-                state.value = list
+        scope.launch {
+            delay(3000)
+            val list = scope.async {
+                readCityList(context)
             }
+            state.value = list.await()
         }
     }
     return state.value
 }
 
 
-private fun readCityList(context: Context): List<City> {
+private suspend fun readCityList(context: Context): List<City> = coroutineScope {
     //I want to use kotlin-serializer, but "Backend code generation error" has occurred at compile time.
     //Same error happened when I use "kotshi".
     //Probably, this is the issue of Android-Studio-Preview.
     //So I switched to Gson.
 
-    return context.resources.assets.open("city_list.json").use {
-        val jsonText = it.bufferedReader().readText()
-        val gson = Gson()
-        val listType = object : TypeToken<List<City>>() {}.type
-        gson.fromJson<List<City>>(jsonText, listType)
-    } ?: emptyList<City>()
+    val jsonText = readAsset(context, "city_list.json")
+    val gson = Gson()
+    val listType = object : TypeToken<List<City>>() {}.type
+    val result = gson.fromJson<List<City>>(jsonText, listType)
+    result ?: emptyList<City>()
 }
--- app/src/main/java/com/github/naodroid/jetweather/data/state/Resourse.kt
+++ app/src/main/java/com/github/naodroid/jetweather/data/state/Resourse.kt
@@ -1,18 +1,11 @@
 package com.github.naodroid.jetweather.data.state
 
 import android.content.Context
-import androidx.compose.ambient
-import androidx.compose.modelFor
-import androidx.compose.state
-import androidx.compose.unaryPlus
-import androidx.ui.core.ContextAmbient
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.coroutineScope
 
 
-fun readAsset(context: Context, assetName: String): String? {
-    return context.assets.open(assetName).use {
+suspend fun readAsset(context: Context, assetName: String): String? = coroutineScope {
+    context.assets.open(assetName).use {
         it.bufferedReader().readText()
     }
 }
